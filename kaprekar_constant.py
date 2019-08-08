a64bc4bdd0794c199cbc87cbb90a0a4d8fb60ed6
Alden Davidson <adavidson@protonmail.ch>
2019-08-08 16:09:35 -0400

    initial python file

---
+++ kaprekar_constant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Calculates and outputs the steps taken to reach Kaprekar's Constant given any
+4-digit number. The only restrictions placed on the provided number is that it
+contain at least two unique digits, otherwise a separate endstate is reached.
+
+Author: Alden Davidson
+Email: adavidson@pm.me
+"""
